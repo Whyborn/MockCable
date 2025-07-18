f748a489f906ee8ca63c9fa9fb107eafa76180c4
Whyborn <lachlan.s.whyborn@gmail.com>
2025-07-18 15:46:08 +1000

    Clean up the testing structure

--- test/mock_tests.F90
+++ test/mock_tests.F90
@@ -1,9 +1,8 @@
 MODULE mock_tests
 
-  USE fortuno_interface_m, ONLY: IS_EQUAL, TEST_CASE,&
-    CHECK, SUITE
-  USE common_tests
-  USE time_tests
+  use fortuno_interface_m, only: test_list
+  use common_tests, only: common_test_list
+  use time_tests, only: time_test_list
   !USE datasetreader_tests
   !USE output_tests
 
@@ -15,17 +14,9 @@ CONTAINS
     TYPE(TEST_LIST) :: Tests
 
     Tests = TEST_LIST([&
-      SUITE("common", TEST_LIST([&
-        TEST_CASE("test_sort", test_sort),&
-        TEST_CASE("test_approx_equal", test_approx_equal),&
-        TEST_CASE("test_find_largest_less_than", test_find_largest_less_than)&
-        ])),&
-      SUITE("time", TEST_LIST([&
-        TEST_CASE("test_calendars", test_calendars),&
-        TEST_CASE("test_to_from_string", test_to_from_string),&
-        TEST_CASE("test_intervals", test_intervals)&
-        ]))&
-      ])
+      common_test_list(),&
+      time_test_list()&
+    ])
 
   END FUNCTION tests
 
